943040d0ea1cbede5a0c1935aa9eb02c084d4d85
Delvina <dellevee00@gmail.com>
2026-05-31 22:22:07 +0700

    membuat profil bisa ganti foto

--- resources/views/profile/partials/update-profile-information-form.blade.php
+++ resources/views/profile/partials/update-profile-information-form.blade.php
@@ -17,11 +17,31 @@
         @method('patch')
 
         <div>
-            <x-input-label for="profile_photo" :value="__('FOTO PROFIL')" class="text-gray-300" />
-            <input id="profile_photo" name="profile_photo" type="file" class="mt-1 block w-full text-white bg-[#1a192f] border-gray-700 p-2 rounded" accept="image/*" />
+            <x-input-label :value="__('FOTO PROFIL')" class="text-gray-300 mb-3" />
+            
+            <div class="flex items-center gap-6">
+                <div class="relative w-24 h-24">
+                    <div class="w-full h-full bg-gradient-to-br from-purple-700 to-indigo-900 rounded-2xl flex items-center justify-center text-white text-4xl font-black shadow-lg border border-purple-500/30">
+                        {{ strtoupper(substr($user->name, 0, 1)) }}
+                    </div>
+
+                    <label for="profile_photo" class="absolute -bottom-2 -right-2 bg-blue-600 hover:bg-blue-500 text-white w-8 h-8 rounded-full flex items-center justify-center cursor-pointer shadow-md transition-all duration-200 hover:scale-110 border-2 border-[#1a192f]">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" viewBox="0 0 20 20" fill="currentColor">
+                            <path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z" />
+                        </svg>
+                    </label>
+
+                    <input id="profile_photo" name="profile_photo" type="file" class="hidden" accept="image/*" />
+                </div>
+
+                <div class="text-xs text-gray-400 space-y-1">
+                    <p class="font-bold text-gray-300">Klik ikon pensil untuk mengubah foto.</p>
+                    <p>Format: JPG, PNG atau GIF (Maks. 2MB)</p>
+                </div>
+            </div>
+            
             <x-input-error class="mt-2" :messages="$errors->get('profile_photo')" />
         </div>
-
         <div>
             <x-input-label for="name" :value="__('NAMA LENGKAP')" class="text-gray-300" />
             <x-text-input id="name" name="name" type="text" class="mt-1 block w-full bg-[#1a192f] text-white border-gray-700" :value="old('name', $user->name)" required autofocus autocomplete="name" />
