d32923f99765f9bf66a02dcf53c19cfd58ae3c05
Aldo <aldoriantama4@gmail.com>
2026-06-29 19:05:31 +0700

    add demo user seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,23 +3,38 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
-    use WithoutModelEvents;
-
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // User::factory(10)->create();
+        User::updateOrCreate(
+            ['email' => 'admin@etixis.com'],
+            [
+                'name' => 'Admin E-TIXIS',
+                'password' => Hash::make('password'),
+                'role' => 'admin',
+            ]
+        );
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        User::updateOrCreate(
+            ['email' => 'user@etixis.com'],
+            [
+                'name' => 'User E-TIXIS',
+                'password' => Hash::make('password'),
+                'role' => 'user',
+            ]
+        );
+
+        User::updateOrCreate(
+            ['email' => 'petugas@etixis.com'],
+            [
+                'name' => 'Petugas E-TIXIS',
+                'password' => Hash::make('password'),
+                'role' => 'petugas',
+            ]
+        );
     }
 }
